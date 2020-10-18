0569440a73822218b762c3882c70d0cc7976bbba
demkom58 <demkom58@gmail.com>
2020-10-18 07:11:52 +0300

    Workable state of JSGF voice recognition

--- src/main/java/com/css/cleo/Main.java
+++ src/main/java/com/css/cleo/Main.java
@@ -1,7 +1,34 @@
 package com.css.cleo;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final Configuration config = new Configuration();
+        final String resAudioRec = "resource:/audio_recognition";
+        config.setSampleRate(8000);
+        config.setAcousticModelPath(resAudioRec + "/hmm/ru3");
+        config.setDictionaryPath(resAudioRec + "/dictionary/ru.dic");
+        config.setGrammarPath(resAudioRec + "/lang/");
+        config.setUseGrammar(true);
+        config.setGrammarName("text");
+
+        final StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(config);
+
+        final File file = new File("L:/IdeaProjects/cleo/src/main/resources/audio_recognition/test/audio1.wav");
+        final FileInputStream fileInputStream = new FileInputStream(file);
+        recognizer.startRecognition(fileInputStream);
+
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null)
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+
+        recognizer.stopRecognition();
     }
 }
