2df27b2ae63853e7759452b73b89fe7b3566aa90
demkom58 <demkom58@gmail.com>
2020-10-18 15:10:31 +0300

    Cleo tray icon snippet

--- src/main/java/com/css/cleo/Main.java
+++ src/main/java/com/css/cleo/Main.java
@@ -4,12 +4,16 @@ import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        setTrayIcon();
+
         final Configuration config = new Configuration();
         final String resAudioRec = "resource:/audio_recognition";
         config.setSampleRate(8000);
@@ -30,5 +34,31 @@ public class Main {
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
 
         recognizer.stopRecognition();
+        Thread.sleep(1000 * 20);
+    }
+
+
+    private static void setTrayIcon() {
+        if (!SystemTray.isSupported())
+            return;
+
+        PopupMenu trayMenu = new PopupMenu();
+        MenuItem item = new MenuItem("Exit");
+        item.addActionListener(e -> System.exit(0));
+        trayMenu.add(item);
+
+        URL imageURL = Main.class.getResource("/assets/icon/cleo-x256.png");
+        Image icon = Toolkit.getDefaultToolkit().getImage(imageURL);
+        TrayIcon trayIcon = new TrayIcon(icon, "Cleo", trayMenu);
+        trayIcon.setImageAutoSize(true);
+
+        SystemTray tray = SystemTray.getSystemTray();
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        trayIcon.displayMessage("Cleo", "Application started!", TrayIcon.MessageType.INFO);
     }
 }
