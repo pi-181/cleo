f0075e5761ed21355e67fa3f53fe69ebe1892598
demkom58 <demkom58@gmail.com>
2020-10-22 07:22:51 +0300

    Compilation bug fix

--- src/main/java/com/css/cleo/Main.java
+++ src/main/java/com/css/cleo/Main.java
@@ -2,8 +2,10 @@ package com.css.cleo;
 
 import com.css.cleo.util.Keyboard;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Keyboard.init();
         new CleoApplication().start();
     }
