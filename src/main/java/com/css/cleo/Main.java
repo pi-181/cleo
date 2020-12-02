0089177b605d6842130c7711a86cf41e4e6efe72
lugovskyi <54972818+lugovskyi@users.noreply.github.com>
2020-12-02 19:02:18 +0200

    remade comments for build.gradle

--- src/main/java/com/css/cleo/Main.java
+++ src/main/java/com/css/cleo/Main.java
@@ -2,10 +2,8 @@ package com.css.cleo;
 
 import com.css.cleo.util.Keyboard;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Keyboard.init();
         new CleoApplication().start();
     }
