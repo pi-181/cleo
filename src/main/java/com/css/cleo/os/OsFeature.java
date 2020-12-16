256dae390d8f2dce0ab5469d4977a669b7e64bbe
demkom58 <demkom58@gmail.com>
2020-12-16 16:45:34 +0200

    Added simple default open in browser method

--- src/main/java/com/css/cleo/os/OsFeature.java
+++ src/main/java/com/css/cleo/os/OsFeature.java
@@ -2,7 +2,12 @@ package com.css.cleo.os;
 
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 
 public interface OsFeature {
@@ -25,4 +30,20 @@ public interface OsFeature {
      */
     @NotNull
     Collection<File> getDesktopApps();
+
+    /**
+     * Opens in browser website by provided url.
+     *
+     * @param url address of page
+     */
+    default void executeBrowser(String url) {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
+            }
+        }
+    }
 }
