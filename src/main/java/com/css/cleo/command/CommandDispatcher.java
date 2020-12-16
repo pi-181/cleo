ffb73bd1314d3a2c501d8009f4ba4e3a0cd8e839
demkom58 <demkom58@gmail.com>
2020-12-16 20:49:31 +0200

    Better logic of executing commands

--- src/main/java/com/css/cleo/command/CommandDispatcher.java
+++ src/main/java/com/css/cleo/command/CommandDispatcher.java
@@ -4,7 +4,9 @@ import com.css.cleo.os.OsFeature;
 import com.css.cleo.voice.language.LanguageManager;
 import edu.cmu.sphinx.api.SpeechResult;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CommandDispatcher {
     private final CommandRegistry commandRegistry = new CommandRegistry();
@@ -17,16 +19,18 @@ public class CommandDispatcher {
     }
 
     public boolean dispatch(SpeechResult result) {
-        for (String rule : languageManager.getRules(result.getHypothesis())) {
-            Optional<Command> command = commandRegistry.getCommand(rule);
-            if (command.isEmpty())
-                continue;
+        String hypothesis = result.getHypothesis();
+        List<Command> commands = languageManager.getRules(hypothesis).stream()
+                .map(commandRegistry::getCommand)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
 
-            command.get().execute(osFeature, result);
-            return true;
-        }
+        if (commands.isEmpty() || commands.size() > 1)
+            return false;
 
-        return false;
+        commands.get(0).execute(osFeature, result);
+        return true;
     }
 
 }
